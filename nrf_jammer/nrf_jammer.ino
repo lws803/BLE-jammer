49cc32fe6714bfd96a7ff6b075c4246401dc259d
lws803 <lws803@gmail.com>
2020-06-27 23:56:23 +0800

    updated code

--- nrf_jammer/nrf_jammer.ino
+++ nrf_jammer/nrf_jammer.ino
@@ -10,25 +10,28 @@
 #include <RF24.h>
 
 
-RF24 radio(PC14, PC13); // CE, CSN
+RF24 radio(PC14, PA3); // CE, CSN
 RF24 radio2(PA0, PA1);
-RF24 radio3(PA2, PA3);
+RF24 radio3(PA2, PB0);
 
 void setup() {
   pinMode(D33, OUTPUT);
+  digitalWrite(D33, HIGH);
 
   radio.begin();
   radio.setAutoAck(false);  // Very important setting
   radio.setPALevel(RF24_PA_HIGH);
   radio.setDataRate(RF24_2MBPS);
   radio.stopListening();
-
+  radio.setChannel(80);
   delay(1000);
+
   radio2.begin();
   radio2.setAutoAck(false);
   radio2.setPALevel(RF24_PA_HIGH);
   radio2.setDataRate(RF24_2MBPS);
   radio2.stopListening();
+  radio2.setChannel(26);
   delay(1000);
 
   radio3.begin();
@@ -36,18 +39,16 @@ void setup() {
   radio3.setPALevel(RF24_PA_HIGH);
   radio3.setDataRate(RF24_2MBPS);
   radio3.stopListening();
+  radio3.setChannel(2);
   delay(1000);
 
-  digitalWrite(D33, HIGH);
+  digitalWrite(D33, LOW);
 }
 
 
 void loop() {
   const char text[] = "x"; //just some random string
-  radio.setChannel(80);
   radio.writeFast(&text, sizeof(text));
-  radio3.setChannel(2);
   radio3.writeFast(&text, sizeof(text));
-  radio2.setChannel(26);
   radio2.writeFast(&text, sizeof(text));
 }
